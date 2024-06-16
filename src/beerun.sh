42e0725b643a6b9ada5962e90ed5cc3596737707
fabberr <fabriciomilanez@protonmail.com>
2024-06-16 11:14:26 -0300

    Bump version number

--- src/beerun.sh
+++ src/beerun.sh
@@ -58,7 +58,7 @@ function display_help {
 
 function display_version {
     echo "File:           beerun.sh"
-    echo "Version:        1.0.1"
+    echo "Version:        1.0.2"
     echo "Author:         https://github.com/fabberr"
     echo "Description:    Build and run solutions for https://judge.beecrowd.com problems implemented in C++."
     echo "Licence:        GPLv3 (https://www.gnu.org/licenses/gpl-3.0.txt)"
